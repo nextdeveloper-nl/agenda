22a44a274f27c9f6c397615eaf0857eef3138a82
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-04-03 17:20:17 +0300

    Authorization levels changed

--- src/Authorization/Roles/AgendaAdminRole.php
+++ src/Authorization/Roles/AgendaAdminRole.php
@@ -15,7 +15,7 @@ class AgendaAdminRole extends AbstractRole implements IAuthorizationRole
 {
     public const NAME = 'agenda-admin';
 
-    public const LEVEL = 50;
+    public const LEVEL = 100;
 
     public const DESCRIPTION = 'Agenda Admin';
 
